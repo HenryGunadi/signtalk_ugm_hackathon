c8d5d850443d47c9b3ca294e5ab4291877d5e4d3
HenryGunadi <henrywilliamgunadi09@gmail.com>
2025-03-28 06:49:31 +0700

    tambah handlers

--- backend/server.py
+++ backend/server.py
@@ -14,13 +14,18 @@ async def sendError(websocket, message: str):
 
     await websocket.send(json.dumps(event))
 
-async def broadcastMessage(room, message) -> None:
+async def broadcastMessage(room: 'Room', message, *args) -> None:
     try:
-        broadcast(room, message)
+        # broadcast to other people excluding the user itself
+        if args:
+            broadcast([list(filter(lambda user: user.websocket != args, room.connected_participants))], message)
+        else:
+            # broadcast to all users in the room
+            broadcast([user.websocket for user in room.connected_participants], message)
     except Exception as e:
         print(f"Unexpected error : {e}")
         raise
-
+    
 # === Classes ===
 class Room():
     def __init__(self, id):
@@ -32,12 +37,19 @@ class Room():
             if new_user.id == user.id:
                 await new_user.websocket.send("User already exists in the room.")
 
+                return
+
         self.connected_participants.append(new_user)
 
     async def removeUser(self, websocket) -> None:
         self.connected_participants = [
             user for user in self.connected_participants if user.websocket != websocket 
-        ]    
+        ]
+
+    # DEMO ONLY FOR 2 USERS FIRST
+    async def handleICEAndSDP(self):
+        pass
+
 
 class User():
     def __init__(self, id, websocket, room: Room):
@@ -50,7 +62,7 @@ class User():
             if not isinstance(message, str):
                 raise TypeError("Message is not a string!")
             
-            # else broadcast to the room
+            # else broadcast to the room including him/her self
             broadcastMessage(self.room, message)
         except TypeError as e:
             print(str(e))
@@ -64,6 +76,22 @@ class User():
     async def leaveRoom(self) -> None:
         self.room.removeUser(self.websocket)
 
+    # user in the call
+    async def inCall(self) -> None:
+        while True:
+            try:
+                for messsage in self.websocket:
+                    pass
+            finally:
+                try:
+                    # user disconnects
+                    self.room.removeUser(self.websocket)
+                    broadcastMessage(self.room, f"User : {self.id} disconnected.", self.websocket)
+                    del self
+                except Exception as e:
+                    print(f"Broadcast error : {e}")
+                    del self
+
 class App():
     def __init__(self):
         self.rooms: List[Room] = []
@@ -74,13 +102,15 @@ class App():
     def deleteRoom(self, room_id: str) -> None:
         self.rooms = [
             room for room in self.rooms if room.room_id != room_id
-        ]        
+        ]
 
 class Server():
     def __init__(self, app: App):
         self.app: App = app
-
+    
+    # start the websocket server
     async def run(self) -> None:
+        # websocket is passed here
         async with serve(self.handler, "localhost", 8000) as server:
             print("Websocket server is running on port 8000")
             await server.serve_forever()
@@ -95,8 +125,11 @@ class Server():
             user = User(event.get("id"), websocket, room)
             room.addUser(user)
             self.app.addRooms(room)
+            
+            # concurrent functions
+            # handle incoming users to handle ice and sdp
 
-            # user is in a call
+            # user is in the call
             
         elif event.get("type") == "join":
             room_id = event.get("room_id")
@@ -106,6 +139,8 @@ class Server():
                 if room.room_id == room_id:
                     user = User(event.get("id"), websocket, room)
                     room.addUser(user)
+
+            # user joined the call
         else:
             logs = "Event type not found!" 
             print(logs)
