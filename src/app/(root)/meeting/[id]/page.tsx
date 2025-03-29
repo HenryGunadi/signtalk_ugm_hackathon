a8e02f78a60a6034dcb913c372b76b3c5095c9f6
BRYAN1309 <bryanthanaya1@gmail.com>
2025-03-29 21:40:51 +0700

    Update

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Meeting = ({params}: { params: { id: string}}) => {
   return (
-    <div>
+    <div className=' text-white'>
       Meeting Room: #{params.id}
     </div>
   )
