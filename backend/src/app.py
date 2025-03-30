40eb863dda1941afefa9251b6cda989905314b59
HenryGunadi <henrywilliamgunadi09@gmail.com>
2025-03-30 11:55:32 +0700

    testing deploys

--- backend/src/app.py
+++ backend/src/app.py
@@ -50,4 +50,5 @@ def index():
     return render_template("index.html", meetings=data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(debug=True, port=port, host="0.0.0.0")
