ca4ed046e7012f1038aa4c7e62496bcb38f1df83
BRYAN1309 <bryanthanaya1@gmail.com>
2025-04-02 20:32:17 +0700

    Update

--- src/components/MeetingTypeList.tsx
+++ src/components/MeetingTypeList.tsx
@@ -1,10 +1,17 @@
+'use client'
 import React from 'react'
+import Image from 'next/image'
 
 const MeetingTypeList = () => {
   return (
-    <div>
-      
+   <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+    <div className=' bg-orange-400 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer'
+    onClick={()=>{}}>
+      <div className='flex-center glassmorphism size-12 rounded-[10px]'>
+        <Image src='/icons/add-meeting.svg'  alt='meeting' width={27} height={27}/>
+      </div>
     </div>
+   </section>
   )
 }
 
