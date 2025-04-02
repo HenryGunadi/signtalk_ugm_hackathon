83bb5a03af156fb6d9075ca32c6c9d1235385538
BRYAN1309 <bryanthanaya1@gmail.com>
2025-04-02 17:00:56 +0700

    Update

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,34 +1,46 @@
-'use client'
-
-import React from 'react'
-import { sidebarLinks } from '../../constants'
-import { usePathname } from 'next/navigation'
+'use client';
+import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 import { cn } from '@/lib/utils';
+import { sidebarLinks } from '../../constants';
 
 const Sidebar = () => {
-    const pathname = usePathname();
+  const pathname = usePathname();
+
   return (
-    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between p-6 pt-28 bg-blue-950
-     text-white max-sm:hidden lg:w-[264px]'>
-        <div className='flex flex-1 flex-col gap-6'>
-            {sidebarLinks.map((link) =>{
-                const isActive  = pathname === link.route || pathname.startsWith(link.route);
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col  justify-between  bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((item) => {
+          const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
+          
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn(
+                'flex gap-4 items-center p-4 rounded-lg justify-start',
+                {
+                  'bg-blue-1': isActive,
+                }
+              )}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-lg font-semibold max-lg:hidden">
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-                return(
-                    <Link
-                        href={link.route}
-                        key={link.label}
-                        className= {cn('flex gap-4 items-center p-4 rounded-lg justify-start',{ isActive})}
-                    ><p className="text-lg font-semibold max-lg:hidden">
-                    {link.label}
-                     </p> 
-                     </Link>
-                )
-            })}
-        </div>
-        </section>
-  )
-}
-
-export default Sidebar
+export default Sidebar;
