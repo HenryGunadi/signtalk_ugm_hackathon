07db3256e6777f53600e1383a9ca5c59e71d28c7
BRYAN1309 <bryanthanaya1@gmail.com>
2025-04-03 11:33:29 +0700

    Update

--- src/components/MeetingTypeList.tsx
+++ src/components/MeetingTypeList.tsx
@@ -1,14 +1,18 @@
 'use client'
 import React, { useState } from 'react'
-
 import HomeCard from '../components/HomeCard';
 import { useRouter } from 'next/navigation';
+import MeetingModal from './MeetingModal';
 
 const MeetingTypeList = () => {
   const router = useRouter
   const [meetingState, setMeetingState] = useState<
   'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
->(undefined);
+  >(undefined);
+
+  const createMeeting = () =>{
+    
+  }
   return (
     
    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
@@ -39,6 +43,12 @@ const MeetingTypeList = () => {
         className="bg-yellow-600"
         // handleClick={() => router.push('/recordings')}
       />
+       <MeetingModal
+          isOpen={meetingState === 'isScheduleMeeting'}
+          onClose={() => setMeetingState(undefined)}
+          title="Create Meeting"
+          handleClick={createMeeting}
+        ></MeetingModal>
    </section>
   )
 }
